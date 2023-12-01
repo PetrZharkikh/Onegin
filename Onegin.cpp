eb54ea7719632f37b428da79d34af00320a85ffc
PetrZharkikh <142933608+PetrZharkikh@users.noreply.github.com>
2023-12-01 12:53:49 +0300

    Add files via upload
    Bugged swap

--- Onegin.cpp
+++ Onegin.cpp
@@ -3,100 +3,103 @@
 #include <string.h>
 #include <stdlib.h>
 
-size_t count_lines(const char* filename);
-void bsort(char* arr[], size_t size);
+size_t count_lines(char* Arr);
+void bsort(char* arr[], size_t sizeofarr, size_t sizeoftype, int (*cmp)(const void*, const void*));
 int strcmp_reversed(char arr1[], char arr2[]);
 void bsort_reversed(char* arr[], size_t size);
-
-const int buffer_length = 200;
+size_t sizeof_file(FILE * fileName);
+void swap(void* ptr1, void* ptr2, size_t size, size_t sizeofelem);
+int mystrcmp(const void* ptr1, const void* ptr2);
 
 
 int main(void)
     {
+
     FILE* fileOnegin = fopen("file.txt", "r");
 
-    //printf("red\n");
-
-    size_t nLines = count_lines("file.txt");
-
-    char** arrPointer = (char**)calloc(nLines, sizeof(char*));
-    char buffer[buffer_length] = {};
-
-    //printf("%d\n", strlen(buffer[1]));
-    //printf("nLines = %d\n", nLines);
-
-    for (int unsigned i = 0; i < nLines; i++)
+    if (fileOnegin == NULL)
         {
-        fgets(buffer, buffer_length*sizeof(char), fileOnegin);
-        size_t bufSize = strlen(buffer)+1;
-        char* StrMem = (char*)calloc(bufSize, sizeof(char*));
-
-        //strcpy(StrMem, buffer);
-
-        arrPointer[i] = strcpy(StrMem, buffer);
-
-        //printf("%s", arrPointer[i]);
+        printf("File did not open");
+        return 0;
         }
+
+    size_t tsize = sizeof_file(fileOnegin);
+
+    rewind(fileOnegin);
+
+    char* big_buffer = (char*)calloc(tsize+1, sizeof(char));
+    fread(big_buffer, sizeof(char), tsize, fileOnegin);
+
 
     fclose(fileOnegin);
 
-    bsort(arrPointer, nLines);
+    size_t nLines = count_lines(big_buffer);
 
-    //printf("end");
+    char** arrPointer = (char**)calloc(nLines, sizeof(char*));
+
+    arrPointer[0] = &big_buffer[0];
+    for (unsigned int i = 1; i<nLines; i++)
+        {
+        arrPointer[i] = strchr(arrPointer[i-1], '\0') + 1;
+        if (*arrPointer[i] == '\n')
+            arrPointer[i]++;
+        }
+
+    bsort(arrPointer, nLines, sizeof(char*), mystrcmp);
 
     for (int unsigned j = 0; j < nLines; j++)
         {
         printf("%s", arrPointer[j]);
         putchar('\n');
         }
-
-    printf("-----------------------------------------\n");
-
-    bsort_reversed(arrPointer, nLines);
-
-    for (int unsigned j = 0; j < nLines; j++)
-        {
-        printf("%s", arrPointer[j]);
-        putchar('\n');
-        }
-
-    //printf("%d ", strlen(arrPointer[2]));
 
     return 0;
     }
 
 
-size_t count_lines(const char* filename)
-{
-   size_t cnt = 0;
-   char buffer[buffer_length] = {};
-
-   FILE*fp;
-   if((fp = fopen(filename, "r")) == NULL)
-       return 0;
-   while (!feof(fp))
-   {
-     fgets(buffer, buffer_length*sizeof(char), fp);
-     cnt++;
-   }
-   fclose(fp);
-   return cnt;
-}
-
-
-void bsort(char* arr[], size_t size)
+size_t count_lines(char* Arr)
     {
-     for (int unsigned pass = 0; pass < size-1; pass++)
+   int i = 0;
+   int cnt = 0;
+   while (Arr[i]!='\0')
+        {
+        if (Arr[i] == '\r')
+            {
+            Arr[i] = '\0';
+            cnt++;
+            i+=2;
+            continue;
+            }
+        if (Arr[i] == '\n')
+            {
+            Arr[i] = '\0';
+            cnt++;
+            }
+        i++;
+        }
+    if(Arr[i - 1] != '\0' && Arr[i - 1] != '\n')
+        cnt++;
+    return cnt;
+    }
+
+//int str_cmp(const void* lhs, const void* rhs)
+//{
+  //  return strcmp();
+//}
+
+
+//void bsort(char* arr[], size_t size, int (*cmp)(const void*, const void*))
+void bsort(char* arr[], size_t sizeofarr, size_t sizeoftype, int (*cmp)(const void*, const void*))
+    {
+     for (int unsigned pass = 0; pass < sizeofarr - 1; pass++)
         {
          int scr = 0;
-         for (int unsigned i = 0; i < size -1 -pass; i++)
+         for (int unsigned i = 0; i < sizeofarr - 1 -pass; i++)
             {
-             if (strcmp(arr[i],arr[i+1])>0)
+             if (cmp(arr[i], arr[i + 1]) > 0)
                 {
                  scr++;
-                 char* temp = arr[i];
-                 arr[i] = arr[i+1];
-                 arr[i+1] = temp;
+                 swap(arr[i], arr[i+1], 8 , sizeoftype);       //!!!!!!!
                 }
             }
          if (scr == 0) break;
@@ -123,6 +126,20 @@ void bsort_reversed(char* arr[], size_t size)
         }
     }
 
+int mystrcmp(const void* ptr1, const void* ptr2)
+    {
+    int pos = 0;
+
+    const char* str1 = (const char*)ptr1;
+    const char* str2 = (const char*)ptr2;
+
+    while (str1[pos] != 0 && str2[pos] != 0 &&  str1[pos] == str2[pos])
+        {
+        pos++;
+        }
+
+    return (str1[pos]) - (str2[pos]);
+    }
 
 int strcmp_reversed(char s1[], char s2[])
     {
@@ -140,4 +157,25 @@ int strcmp_reversed(char s1[], char s2[])
     if (s1[pos1]>s2[pos2]) return 1;
     if (s1[pos1]<s2[pos2]) return -1;
     return 0;
+    }
+
+
+size_t sizeof_file(FILE * fileName)
+{
+    fseek(fileName, 0, SEEK_END);
+    size_t size = ftell(fileName);
+    return size;
+}
+
+
+void swap(void* ptr1, void* ptr2, size_t size, size_t sizeofelem)
+    {
+    void* temp = (void*)calloc(size, sizeofelem);
+    memcpy(temp, ptr1, size);
+    //printf("%s\n", temp);
+    memcpy(ptr1, ptr2, size);
+    //printf("%s\n", ptr1);
+    memcpy(ptr2, temp, size);
+    //printf("%s\n", ptr2);
+    free(temp);
     }
